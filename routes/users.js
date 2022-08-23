b32e1f021629e4f3fabad988f95a7a87e940a6a1
zhy2on <dhwlgus5940@gmail.com>
2022-08-24 04:55:53 +0900

    Feat: follow, unfollow api 구현

--- routes/users.js
+++ routes/users.js
@@ -25,9 +25,73 @@ router.put("/:id", async (req, res) => {
 		return res.status(403).json("You can update only your account!");
 	}
 });
+
 //delete user
+router.delete("/:id", async (req, res) => {
+	if (req.body.userId === req.params.id || req.body.isAdmin) {
+		try {
+			await User.findByIdAndDelete(req.params.id);
+			res.status(200).json("Account has been deleted");
+		} catch (err) {
+			return res.status(500).json(err);
+		}
+	} else {
+		return res.status(403).json("You can delete only your account!");
+	}
+});
+
 //get a user
+router.get("/:id", async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		const { password, updatedAt, ...other } = user._doc;
+		res.status(200).json(other);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 //follow a user
+router.put("/:id/follow", async (req, res) => {
+	if (req.body.userId !== req.params.id) {
+		try {
+			const user = await User.findById(req.params.id);
+			const currentUser = await User.findById(req.body.userId);
+			if (!user.followers.includes(req.body.userId)) {
+				await user.updateOne({ $push: { followers: req.body.userId } });
+				await currentUser.updateOne({ $push: { followings: req.params.id } });
+				res.status(200).json("User has been followed");
+			} else {
+				res.status(403).json("You already follow this user");
+			}
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	} else {
+		res.status(403).json("You can not follow yourself");
+	}
+});
+
+router.put("/:id/unfollow", async (req, res) => {
+	if (req.body.userId !== req.params.id) {
+		try {
+			const user = await User.findById(req.params.id);
+			const currentUser = await User.findById(req.body.userId);
+			if (user.followers.includes(req.body.userId)) {
+				await user.updateOne({ $pull: { followers: req.body.userId } });
+				await currentUser.updateOne({ $pull: { followings: req.params.id } });
+				res.status(200).json("User has been unfollowed");
+			} else {
+				res.status(403).json("You don't follow this user");
+			}
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	} else {
+		res.status(403).json("You can not unfollow yourself");
+	}
+});
+
 //unfollow a user
 
 module.exports = router
